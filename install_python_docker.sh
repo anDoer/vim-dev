ef9f32f76a2d2c710421c04e2bd1980d65f03bba
Andreas Doering <16701672+anDoer@users.noreply.github.com>
2022-01-16 20:18:43 +0100

    docker install

--- install_python_docker.sh
+++ install_python_docker.sh
@@ -5,12 +5,12 @@ HOME_FOLDER=${1:-$HOME}
 sudo apt-get update
 
 # we need to add current version of nodejs -> ubuntu 20.04 does not offer latest versions 
-#curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-#sudo apt-get install nodejs 
-#
-#pip install --upgrade pynvim
-#conda install -y jedi 
-#
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install nodejs 
+
+pip install --upgrade pynvim
+conda install -y jedi 
+
 ## install packages as non-root user 
 #mkdir "${HOME_FOLDER}/.npm-packages"
 #npm config set prefix "${HOME_FOLDER}/.npm-packages"
