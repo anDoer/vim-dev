fcad7ac687df889f258336330edfbf83acfdfe23
anDoer <16701672+anDoer@users.noreply.github.com>
2022-03-23 13:07:02 +0100

    Ensure latest docker version

--- install_python_docker.sh
+++ install_python_docker.sh
@@ -4,6 +4,12 @@ HOME_FOLDER=${1:-$HOME}
 
 sudo apt-get update
 
+# make sure,  we install the latest version of neovim
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install -y neovim
+
 # we need to add current version of nodejs -> ubuntu 20.04 does not offer latest versions 
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install nodejs 
